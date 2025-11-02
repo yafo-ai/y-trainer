7901bff1b850dfe2f3d88bf8802110a8f9b4b0b9
zhb-ai <zhb0123@126.com>
2025-11-02 23:08:29 +0800

    Reduce training epochs from 3 to 2

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -5,7 +5,7 @@ python -m training_code.start_training \
     --training_type 'cpt' \
     --checkpoint_epoch '0,1,2' \
     --pack_length 1024 \
-    --epoch 3 \
+    --epoch 2 \
     --use_NLIRG \
     --data_path example_dataset/cpt_example.json \
     --output_dir outputdir \
